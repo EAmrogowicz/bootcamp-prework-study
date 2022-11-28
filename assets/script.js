9e86f488d109a8afc6a35e5dca918e38a3f44b28
Ewelina Amrogowicz <ewelina.amrogowicz@gmail.com>
2022-11-28 15:14:33 +0000

    Added JS code to application.

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+//CODE EXCERCISE #1
+// var topics = "HTML, CSS, GIT, JS";
+// console.log(topics);
+
+//CODE EXCERCISE #2
+// var topic = "JS";
+// if (topic === "HTML") {
+//   console.log("Let's study HTML!");
+// } else if (topic === "CSS") {
+//   console.log("Let's study CSS!");
+// } else if (topic === "GIT") {
+//   console.log("Let's study Git!");
+// } else if (topic === "JS" || topic === "JavaScript") {
+//   console.log("Let's study JavaScript!");
+// } else {
+//   console.log("Please try again!");
+// }
+
+//CODE EXCERCISE #3
+var topics = ["HTML", "CSS", "Git", "JavaScript"];
+for (var i = 0; i < topics.length; i++) {
+  console.log(`Let's study ${topics[i]}`);
+}
